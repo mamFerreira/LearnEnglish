8023f9e370b75167c3697eae0b38d67a87a69f51
mamFerreira <mamfer89@gmail.com>
2017-10-27 12:45:48 +0200

    Create Rutas.php

---
+++ clases/Rutas.php
@@ -0,0 +1,27 @@
+<?php
+class Rutas {
+    
+    private $controlador;
+    private $opcion;
+    private $id;
+    
+    function __construct() {
+        
+        $this->controlador = filter_input(INPUT_GET,"controlador"); 
+        $this->opcion = filter_input(INPUT_GET,"opcion");
+        $this->id = filter_input(INPUT_GET,"id"); 
+        
+    }
+    
+    function get_controlador() {
+        return $this->controlador;
+    }
+    
+    function get_opcion() {
+        return $this->opcion;
+    }
+    
+    function get_id() {
+        return $this->id;
+    }
+}
